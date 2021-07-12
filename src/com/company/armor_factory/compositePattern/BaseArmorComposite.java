9b06def5d0ca100e32429c76ed12c5f0ed4ef0ba
FaridAsgarov <asgarovfarid89@gmail.com>
2021-07-12 16:37:42 +0200

    Added ArmorComposite, beginning of Composite pattern

---
+++ src/com/company/armor_factory/compositePattern/BaseArmorComposite.java
@@ -0,0 +1,34 @@
+package com.company.armor_factory.compositePattern;
+
+import com.company.armor_factory.BaseArmor;
+import com.company.armor_factory.Material;
+import java.util.ArrayList;
+
+public class BaseArmorComposite extends BaseArmor {
+
+  public BaseArmorComposite(Material material) {
+    super(material);
+  }
+
+   ArrayList<BaseArmor> ArmorEquipment = new ArrayList<BaseArmor>();
+
+   protected void addArmorList(BaseArmor baseArmor){
+      ArmorEquipment.add(baseArmor);
+   }
+
+   public void removeArmorFromList(BaseArmor baseArmor){
+     ArmorEquipment.remove(baseArmor);
+   }
+
+   public ArrayList<BaseArmor> getChildren(){
+        return this.ArmorEquipment;
+   }
+
+   public int getTotalArmorHP(){
+     int totalHP = 0;
+     for (BaseArmor element : this.getChildren()) {
+       totalHP += element.getArmorHP();
+     }
+     return totalHP;
+   }
+}
