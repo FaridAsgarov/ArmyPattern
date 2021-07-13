479748b20f775403a46120051233778fb555d775
FaridAsgarov <asgarovfarid89@gmail.com>
2021-07-13 16:10:49 +0200

    Added ClothArmorComposite, added 4 subclasses of ClothArmor for 4 bodyparts

---
+++ src/com/company/armor_factory/compositePattern/lightArmor/LightArmorComposite.java
@@ -0,0 +1,14 @@
+package com.company.armor_factory.compositePattern.lightArmor;
+
+import com.company.armor_factory.Material;
+import com.company.armor_factory.compositePattern.BaseArmorComposite;
+
+public class LightArmorComposite extends BaseArmorComposite {
+
+  public LightArmorComposite(Material material) {
+     super(material);
+  }
+
+
+
+}
--- src/com/company/armor_factory/concreteArmorElements/fromCloth/ClothArmguard.java
+++ src/com/company/armor_factory/concreteArmorElements/fromCloth/ClothArmguard.java
@@ -1,10 +1,11 @@
 package com.company.armor_factory.concreteArmorElements.fromCloth;
 
+import com.company.armor_factory.BaseArmor;
 import com.company.armor_factory.materials.Cloth;
 
-public class ClothArmguard {
-      public Cloth cloth;
+public class ClothArmguard extends BaseArmor {
 
       public ClothArmguard() {
+            super(new Cloth());
       }
 }
