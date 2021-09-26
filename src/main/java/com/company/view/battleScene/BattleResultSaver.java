fb70c2a2a00e921e07e34b22a58d90e682fe8374
Javid Asgarov <if19b003@technikum-wien.at>
2021-09-26 19:16:29 +0200

    fixed the BattleResultSaver
    Took 8 minutes

--- src/main/java/com/company/view/battleScene/BattleResultSaver.java
+++ src/main/java/com/company/view/battleScene/BattleResultSaver.java
@@ -2,25 +2,22 @@ package com.company.view.battleScene;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import static com.company.view.Constants.RESOURCES_PATH;
+
 public class BattleResultSaver {
- public BattleResultSaver(String textToWrite) {
-
-   SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-   Date date = new Date();
-//     System.out.println(date.toString());
-    String path = System.getProperty("user.dir") + "\\src\\BattleResultSave.txt";
-    String text = textToWrite;
-
-    try {
-        System.out.println("Saving does not work yet");
-      Files.write(Paths.get("src/resources/myfile.txt"), text.getBytes(), StandardOpenOption.APPEND);
-    } catch (IOException e) {
+    public static void writeLogToFile(String text) {
+        var path = Path.of(RESOURCES_PATH + "/myFile.txt");
+        try (var writer = Files.newBufferedWriter(path)) {
+            writer.write(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-  }
 
 }
