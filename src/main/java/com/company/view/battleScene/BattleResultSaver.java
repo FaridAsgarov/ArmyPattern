d6d8b7151d38b0ef896b398ed43b2053efe67477
Javid Asgarov <if19b003@technikum-wien.at>
2021-09-26 19:27:50 +0200

    formatting added
    Took 6 minutes

--- src/main/java/com/company/view/battleScene/BattleResultSaver.java
+++ src/main/java/com/company/view/battleScene/BattleResultSaver.java
@@ -6,6 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 import static com.company.view.Constants.RESOURCES_PATH;
@@ -14,10 +15,18 @@ public class BattleResultSaver {
     public static void writeLogToFile(String text) {
         var path = Path.of(RESOURCES_PATH + "/myFile.txt");
         try (var writer = Files.newBufferedWriter(path)) {
-            writer.write(text);
+            writer.write(format(text));
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static String format(String text) {
+        return text.replace("null", "")
+                .replace("<html>", "")
+                .replace("</html>", "")
+                .replace("<br>", System.lineSeparator());
+
     }
 
 }
